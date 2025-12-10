af86b087a4f57c1ad0fcb9ae988d7b9def0caa23
Angelo Mendes <angelo030825@gmail.com>
2025-12-10 11:40:14 -0300

    feat: create and authenticate user in `authController.js`

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -2,22 +2,23 @@ import bcrypt from "../utils/bcrypt.js";
 import prisma from "../config/database.js";
 import { generateToken } from "../utils/jwt.js";
 
+// Cria novo usuário
 export const register = async (req, res, next) => {
   try {
-    const { email, password } = req.body;
+    const { email, password, name } = req.body;
 
-    // Validação
     if (!email || !password) {
       return res.status(400).json({ error: "Email e senha são obrigatórios" });
     }
 
-    // verifica se usuário já existe
     const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ error: "Email já cadastrado" });
+    }
 
-    // has da senha
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Cria usuário
+    // cria usuário
     const user = await prisma.user.create({
       data: {
         email,
@@ -29,26 +30,57 @@ export const register = async (req, res, next) => {
         id: true,
         email: true,
         name: true,
-        createAt: true,
+        createdAt: true,
       },
     });
-
-    // gera generateToken
+    // gera token
     const token = generateToken(user.id);
-
     res.status(201).json({ user, token });
   } catch (error) {
     next(error);
   }
 };
 
-// registra usuário com o Google
-// API DO GOOGLE AINDA NÃO ESTÁ IMPLEMENTADA, MAS JÁ DEIXEI A LÓGICA AQUI
+// autenticar usuário
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: "Email e senha são obrigatórios" });
+    }
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user || !user.password) {
+      return res.status(401).json({ errror: "Credenciais inválidas" });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({ error: "Credenciais inválidas" });
+    }
+
+    const token = generateToken(user.id);
+
+    res.json({
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        avatar: user.avatar,
+      },
+      token,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// implementar login com Google
 export const googleAuth = async (req, res, next) => {
   try {
     const { email, name, avatar, googleId } = req.body;
 
-    // Busca ou cria usuário
     let user = await prisma.user.findUnique({ where: { email } });
 
     if (!user) {
@@ -63,7 +95,6 @@ export const googleAuth = async (req, res, next) => {
       });
     }
 
-    // Gera token
     const token = generateToken(user.id);
 
     res.json({
